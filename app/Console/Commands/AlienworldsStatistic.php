b5f3d63804b1ccd8fc1db369be145160a6e5896b
Bogish <ego.cogitare@gmail.com>
2021-11-08 20:23:08 +0200

    ...

--- app/Console/Commands/AlienworldsStatistic.php
+++ app/Console/Commands/AlienworldsStatistic.php
@@ -50,6 +50,8 @@ class AlienworldsStatistic extends AbstractCommand
                     'count' => $data['count'],
                     'date' => $date,
                     'updated_at' => Carbon::now(),
+                    'cpu_usage' => $accountInfo[$account->account]['cpuUsage'],
+                    'cpu_staked' => $accountInfo[$account->account]['cpuStaked'],
                     'last_mine_at' => is_null($lastMine) ? null : \Carbon\Carbon::createFromTimeString($lastMine),
                     'refund_cpu' => $accountInfo[$account->account]['refund']['cpu'] ?? null,
                     'refund_net' => $accountInfo[$account->account]['refund']['net'] ?? null,
