fe6a01883269c0ac3b083125dd1779c045214d0a
Bogish <ego.cogitare@gmail.com>
2021-12-04 15:39:28 +0200

    ...

--- app/Console/Commands/AlienworldsStatistic.php
+++ app/Console/Commands/AlienworldsStatistic.php
@@ -8,6 +8,7 @@ use App\Models\AlienworldsMining;
 use App\Models\Accounts;
 use App\Models\AlienworldsMiningHistory;
 use Carbon\Carbon;
+use Exception;
 use Log;
 
 /**
@@ -33,60 +34,65 @@ class AlienworldsStatistic extends AbstractCommand
         while (true) {
             $date = $this->option('date') ?: date('Y-m-d');
             foreach ($accounts as $account) {
-                $this->info($account->account);
-                $item = AlienworldsMining::whereRaw('date = ? AND account = ?', [$date, $account->account]);
+                try {
+                    $this->info($account->account);
+                    $item = AlienworldsMining::whereRaw('date = ? AND account = ?', [$date, $account->account]);
 
-                /** Fetch mine history */
-                $mines = $wax->earnings([$account->account], $date, $date);
+                    /** Fetch mine history */
+                    $mines = $wax->earnings([$account->account], $date, $date);
 
-                if (empty($mines[$account->account])) {
-                    continue;
-                }
-                $data = $mines[$account->account];
+                    if (empty($mines[$account->account])) {
+                        continue;
+                    }
+                    $data = $mines[$account->account];
 
-                /** Fetch account info */
-                $accountInfo = $wax->accountsInfo([$account->account]);
-                $lastMine = $data['mines'][0]['timestamp'] ?? null;
+                    /** Fetch account info */
+                    $accountInfo = $wax->accountsInfo([$account->account]);
+                    $lastMine = $data['mines'][0]['timestamp'] ?? null;
 
-                $dbData = [
-                    'account' => $account->account,
-                    'total' => $data['total'],
-                    'avg' => $data['avg'],
-                    'count' => $data['count'],
-                    'date' => $date,
-                    'updated_at' => Carbon::now(),
-                    'wax_balance' => $accountInfo[$account->account]['waxBalance'],
-                    'cpu_usage' => $accountInfo[$account->account]['cpuUsage'],
-                    'cpu_staked' => $accountInfo[$account->account]['cpuStaked'],
-                    'last_mine_at' => is_null($lastMine) ? null : Carbon::createFromTimeString($lastMine),
-                    'refund_cpu' => $accountInfo[$account->account]['refund']['cpu'] ?? null,
-                    'refund_net' => $accountInfo[$account->account]['refund']['net'] ?? null,
-                    'refund_ts' => $accountInfo[$account->account]['refund']['timestamp'] ?? null,
-                ];
-                if ($item->count() > 0) {
-                    $this->info('update existing record for ' . $account->account);
-                    $item->update($dbData);
-                } else {
-                    $this->info('add new record for ' . $account->account);
-                    AlienworldsMining::create($dbData);
-                }
+                    $dbData = [
+                        'account' => $account->account,
+                        'total' => $data['total'],
+                        'avg' => $data['avg'],
+                        'count' => $data['count'],
+                        'date' => $date,
+                        'updated_at' => Carbon::now(),
+                        'wax_balance' => $accountInfo[$account->account]['waxBalance'],
+                        'cpu_usage' => $accountInfo[$account->account]['cpuUsage'],
+                        'cpu_staked' => $accountInfo[$account->account]['cpuStaked'],
+                        'last_mine_at' => is_null($lastMine) ? null : Carbon::createFromTimeString($lastMine),
+                        'refund_cpu' => $accountInfo[$account->account]['refund']['cpu'] ?? null,
+                        'refund_net' => $accountInfo[$account->account]['refund']['net'] ?? null,
+                        'refund_ts' => $accountInfo[$account->account]['refund']['timestamp'] ?? null,
+                    ];
+                    if ($item->count() > 0) {
+                        $this->info('update existing record for ' . $account->account);
+                        $item->update($dbData);
+                    } else {
+                        $this->info('add new record for ' . $account->account);
+                        AlienworldsMining::create($dbData);
+                    }
 
-                if (!empty($data['mines'])) {
-                    foreach ($data['mines'] as $mine) {
-                        $entity = [
-                            'account' => $account->account,
-                            'timestamp' => Carbon::createFromTimeString($mine['timestamp']),
-                            'amount' => $mine['amount'],
-                        ];
+                    if (!empty($data['mines'])) {
+                        Log::info('update mining history');
+                        foreach ($data['mines'] as $mine) {
+                            $entity = [
+                                'account' => $account->account,
+                                'timestamp' => Carbon::createFromTimeString($mine['timestamp']),
+                                'amount' => $mine['amount'],
+                            ];
 
-                        /** @var AlienworldsMiningHistory $item */
-                        $item = AlienworldsMiningHistory::whereRaw('account = ? AND timestamp = ?', [$account->account, $entity['timestamp']]);
+                            /** @var AlienworldsMiningHistory $item */
+                            $item = AlienworldsMiningHistory::whereRaw('account = ? AND timestamp = ?', [$account->account, $entity['timestamp']]);
 
-                        /** Update existing or create new record */
-                        if ($item->count() === 0) {
-                            AlienworldsMiningHistory::create($entity);
+                            /** Update existing or create new record */
+                            if ($item->count() === 0) {
+                                AlienworldsMiningHistory::create($entity);
+                            }
                         }
                     }
+                } catch (Exception $e) {
+                    Log::error($e->getMessage());
                 }
 
                 $this->sleep($delay);
